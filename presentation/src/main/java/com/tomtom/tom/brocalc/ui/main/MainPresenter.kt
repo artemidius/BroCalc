19a67b7b2358bc011d764b827833adc5e5277b4f
artemidius <Minsk1981>
2018-03-30 02:53:01 +0300

    Added click handling

--- presentation/src/main/java/com/tomtom/tom/brocalc/ui/main/MainPresenter.kt
+++ presentation/src/main/java/com/tomtom/tom/brocalc/ui/main/MainPresenter.kt
@@ -1,9 +1,35 @@
 package com.tomtom.tom.brocalc.ui.main
 
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import com.tomtom.tom.brocalc.R.id.*
 import com.tomtom.tom.brocalc.base.BasePresenter
 
 class MainPresenter(mainActivity: MainActivity): BasePresenter(), MainContract.Presenter {
+
+    val view:MainContract.View = mainActivity
+
+    override fun onClick(view: View?) {
+        if (view != null) {
+            if (view.tag == "number") {
+                val button = view as TextView
+                Log.i(tag, button.text as String?)
+            } else {
+                when (view.id) {
+                    point -> Log.i(tag, "point")
+                    ac -> Log.i(tag, "AC")
+                    back -> Log.i(tag, "back")
+                    currency_choice_upper -> Log.i(tag, "currency_choice_upper")
+                    currency_choice_lower -> Log.i(tag, "currency_choice_lower")
+                    currency_value_upper -> {
+                        Log.i(tag, "currency_value_upper")
+                    }
+                    currency_value_lower -> Log.i(tag, "currency_value_lower")
+                }
+            }
+        }
+    }
 
     private val tag = this.javaClass.simpleName
 
